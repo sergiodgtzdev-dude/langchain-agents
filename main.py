8c6a84144145ffb81a8727301d3dd9833d0d39dd
Sergio D. Gutierrez <sergiodgtzdev@gmail.com>
2026-02-01 19:14:07 -0600

    ReAct agent finished

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_classic.chains.question_answering.map_rerank_prompt import output_parser
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
-tools =[TavilySearch()]
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
+tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
-react_prompt = hub.pull("hwchase17/react")
 agent = create_react_agent(
-    llm = llm,
-    tools = tools,
-    prompt= react_prompt
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-agent_executor.invoke(
-    input={
-        "input" : "Search for the results of the last 3 Paris Saint Germain matches and do a summary of the score"
-    }
-)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
-    print(react_prompt)
+    result = agent_executor.invoke(
+        input={
+            "input": "Search for the results of the last 3 Paris Saint Germain matches and do a summary of the score, add the sources you used to the final response"
+        }
+    )
+    print(result)
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     main()
